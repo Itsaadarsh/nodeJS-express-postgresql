a7c61e9436055180a6678f584b01c0e351cadb37
Aadarsh <aadarsh.s2019@vitbhopal.ac.in>
2020-08-27 18:17:42 +0530

    new start with sql db

--- localstorageDB/dist/controllers/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const error404 = (_req, res, _next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-};
-exports.default = module.exports = {
-    error404,
-};
-//# sourceMappingURL=error.js.map
--- localstorageDB/src/controllers/error.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const error404 = (_req: express.Request, res: express.Response, _next: express.NextFunction) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-};
-
-export default module.exports = {
-  error404,
-};
